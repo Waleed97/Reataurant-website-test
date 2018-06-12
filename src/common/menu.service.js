d3a87a0193ea0acd040db609b46f5ba8b5bce304
Waleed97 <Waleed97@users.noreply.github.com>
2018-06-13 00:20:07 +0530

    Update menu.service.js

--- src/common/menu.service.js
+++ src/common/menu.service.js
@@ -1,28 +1,22 @@
-// Placed in COmmon folder and module as even admin should access the menu items for updation
-
 (function () {
 "use strict";
 
 angular.module('common')
-.service(MenuService,'MenuService');
+.service('MenuService', MenuService);
 
-MenuService.$inject=['$http'];
-function MenuService($http){
 
-var service=this;
+MenuService.$inject = ['$http', 'ApiPath'];
+function MenuService($http, ApiPath) {
+  var service = this;
 
-service.getCategories=function(){
-  return  $http({
-      method: "GET",
-      url: ("http://davids-restaurant.herokuapp.com/menu_items.json")
-    }).then(function (response) {
-        console.log(response.data);
-        return response.data;
-    }).catch(function (error) {
-        console.log(error);
-      });
-};
+  service.getCategories = function () {
+    return $http.get(ApiPath + '/categories.json').then(function (response) {
+      return response.data;
+    });
+  };
 
 }
+
+
 
 })();
